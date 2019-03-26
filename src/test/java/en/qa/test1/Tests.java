23b955f8135396fca0ebf0252902472f94b6b1b3
StanislavVasilev <48835281+StanislavVasilev@users.noreply.github.com>
2019-03-26 23:47:54 +0300

    add tests

--- src/test/java/en/qa/test1/Tests.java
+++ src/test/java/en/qa/test1/Tests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -23,6 +24,25 @@ public class Tests {
   public void mainpageTest(){
     String pageTitle = wd.findElement(By.xpath("//a/span[contains(text(), 'Маркет')]")).getText();
     Assert.assertEquals(pageTitle, "Маркет");
+  }
+  @Test
+  public void searchResultTest(){
+    wd.findElement(By.id("header-search")).sendKeys("Купить гардероб");
+    wd.findElement(By.xpath("//span[@class='button2__text']/parent::button")).click();
+    String results = wd.findElement(By.xpath("//div[contains(text(), 'найдено')]")).getText();
+    Assert.assertEquals(results, "блаблабла");
+  }
+  @Test
+  public void mainPageTest(){
+    wd.findElement(By.xpath("//span[text() = 'Яндекс']/parent::a")).click();
+    boolean titleMainPage = wd.findElement(By.className("home-logo__default")).isDisplayed();
+    Assert.assertEquals(titleMainPage, true);
+  }
+
+  @AfterMethod
+  public void tearDown(){
+    wd.quit();
+    wd = null;
   }
 
 
